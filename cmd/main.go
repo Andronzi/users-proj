ff1888c48ecedb7847edfc903fd14a7d6c9c905c
a.s.radionov <a.s.radionov@tinkoff.ru>
2025-03-08 20:58:02 +0700

    Add Creating user to role middleware

--- cmd/main.go
+++ cmd/main.go
@@ -63,6 +63,7 @@ func main() {
 	roleSafeMethods := map[string]bool{
 		"/users.v1.InternalUserService/ListUsers":      true,
 		"/users.v1.InternalUserService/AssignAdmin":    true,
+		"/users.v1.InternalUserService/CreateUser":     true,
 		"/users.v1.InternalUserService/CreateEmployee": true,
 		"/users.v1.InternalUserService/BanUser":        true,
 	}
